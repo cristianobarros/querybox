bff10dc9fda9b6ccf3dbb0ddeca696b38146efa9
Cristiano de Barros <cristiano.barros13@gmail.com>
2016-12-19 23:31:45 -0200

    Changed ace from brace package

--- renderer.js
+++ renderer.js
@@ -1,7 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var ace = require("brace");
+
+require("brace/mode/sql");
+require("brace/theme/vibrant_ink");
+require("brace/ext/language_tools");
+require("brace/ext/statusbar");
+
 var editor = ace.edit("editor");
+
 editor.setTheme("ace/theme/vibrant_ink");
 editor.getSession().setMode("ace/mode/sql");
 editor.setFontSize("14px");
@@ -9,5 +17,6 @@ editor.setShowPrintMargin(false);
 editor.setOptions({
 	enableBasicAutocompletion: true
 });
-var StatusBar = ace.require("ace/ext/statusbar").StatusBar;
+
+var StatusBar = ace.acequire("ace/ext/statusbar").StatusBar;
 var statusBar = new StatusBar(editor, document.getElementById("status"));
