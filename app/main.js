8eaa70e0a6f83e849cd8eccf41ef946ae003e3d8
Cristiano de Barros <cristiano.barros13@gmail.com>
2017-03-30 00:24:05 -0300

    Application name adjust

--- app/main.js
+++ app/main.js
@@ -28,7 +28,7 @@ function createWindow () {
 		{
 			width : workArea.width,
 			height : workArea.height,
-			title : 'SQL Editor',
+			title : 'Quantum SQL',
 			icon : './build/icon.ico',
 			show : false,
 			webPreferences : {
