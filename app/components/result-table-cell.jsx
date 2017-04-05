d5e4d45c77f8db8ee075bd494b118b4a4eb64b37
Cristiano de Barros <cristiano.barros13@gmail.com>
2017-04-05 14:57:06 -0300

    Change to white the color of the three dots ('...') when NULL text overflows

--- app/components/result-table-cell.jsx
+++ app/components/result-table-cell.jsx
@@ -5,15 +5,16 @@ import {Cell} from 'fixed-data-table';
 export default class ResultTableCell extends PureComponent {
 
 	valueToHTML (value) {
-		if (value == null) {
-			return <span className="label label-default">NULL</span>;
-		} else if (value instanceof Date) {
+		if (value instanceof Date) {
 			return dateFormat(value, 'yyyy-mm-dd HH:MM:ss.l');
 		}
 		return value;
 	};
 
 	render() {
+		if (this.props.value == null) {
+			return <Cell><div className="cell"><div className="cell-overflow-null"><span className="label label-default">NULL</span></div></div></Cell>;
+		}
 		return <Cell><div className="cell"><div className="cell-overflow">{this.valueToHTML(this.props.value)}</div></div></Cell>;
 	}
 
