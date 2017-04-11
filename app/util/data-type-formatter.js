2a1893770ce1a58bbc0c605c872bfcae86a34a13
Cristiano de Barros <cristiano.barros13@gmail.com>
2017-04-11 08:54:39 -0300

    Fixed bug when the value is null

--- app/util/data-type-formatter.js
+++ app/util/data-type-formatter.js
@@ -4,12 +4,16 @@ import dateFormat from "dateformat";
 
 function DataTypeFormatter() {
 
+	const formatters = {
+		date : (value) => dateFormat(value, "yyyy-mm-dd"),
+		timestampWithoutTimezone : (value) => dateFormat(value, "yyyy-mm-dd HH:MM:ss.l"),
+		timestamp : (value) => dateFormat(value, "yyyy-mm-dd HH:MM:ss.l"),
+		undefined : (value) => String(value)
+	}
+
 	function format(type, value) {
-		const formatters = {
-			date : (value) => dateFormat(value, "yyyy-mm-dd"),
-			timestampWithoutTimezone : (value) => dateFormat(value, "yyyy-mm-dd HH:MM:ss.l"),
-			timestamp : (value) => dateFormat(value, "yyyy-mm-dd HH:MM:ss.l"),
-			undefined : (value) => String(value)
+		if (value == null) {
+			return value;
 		}
 		return formatters[type](value);
 	}
