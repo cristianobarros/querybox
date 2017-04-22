7e7f38d8b79b8e107dacbd6e215401c2d07145a8
Cristiano de Barros <cristiano.barros13@gmail.com>
2017-04-22 20:21:01 -0300

    Updagraded webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports={
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query:{
           presets:['react','es2015']
         }
