bbb96c00f05c5a9e533b1a94bcc8555d127d48c4
Cristiano de Barros <cristiano.barros13@gmail.com>
2017-09-14 01:16:13 -0300

    Class to check for applicaton updates

---
+++ app/util/update-checker.js
@@ -0,0 +1,25 @@
+
+import request from 'request';
+import config from './../package.json';
+
+class UpdateChecker {
+
+	hasUpdateAvaliable() {
+		return new Promise((fulfill, reject) => {
+			const repository = config.repository.replace('https://github.com/', '');
+			request({
+			  url: 'https://api.github.com/repos/' + repository + '/releases/latest',
+			  headers: {
+			    'User-Agent': 'request'
+			  }
+			}, (error, response, body) => {
+				const latestVersion = JSON.parse(body).tag_name;
+				const currentVersion = 'v' + config.version;
+				fulfill(latestVersion !== currentVersion);
+			});
+		});
+	}
+
+}
+
+export default new UpdateChecker();
