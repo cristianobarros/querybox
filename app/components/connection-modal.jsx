5ece11ecadc9c87c24a2eac629d2118b0bc030e6
Cristiano de Barros <cristiano.barros13@gmail.com>
2021-09-12 19:10:30 -0300

    Upgrade ConnectionModal to function component

--- app/components/connection-modal.jsx
+++ app/components/connection-modal.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 
 import $ from 'jquery';
 import 'bootstrap';
@@ -14,110 +14,103 @@ const EMPTY_STATE = {
   "password" : ""
 };
 
-export default class ConnectionModal extends PureComponent {
+const ConnectionModal = React.memo(forwardRef((props, ref) => {
 
-  constructor() {
-    super();
-    this.state = EMPTY_STATE;
-  }
+  const connectionModal = useRef();
+  const [ state, setState ] = useState(EMPTY_STATE);
 
-  componentDidMount() {
-    $(this.refs.connectionModal).modal({
+  useEffect(() => {
+    $(connectionModal.current).modal({
       show: false,
       keyboard: false,
       backdrop: "static"
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="modal fade" role="dialog" ref="connectionModal">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" onClick={() => this.cancel()}>&times;</button>
-              <h4 className="modal-title">Connection</h4>
-            </div>
-            <div className="modal-body">
-              <div className="form-group">
-                <label htmlFor="type" className="control-label">Type</label>
-                <select id="type" value={this.state.type} className="form-control" onChange={(event) => this.handleChange(event)}>
-                  <option value=""></option>
-                  <option value="MySQL">MySQL</option>
-                  <option value="PostgreSQL">PostgreSQL</option>
-                  <option value="SQLServer">SQLServer</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <label htmlFor="host" className="control-label">Host</label>
-                <input type="text" id="host" value={this.state.host} className="form-control" onChange={(event) => this.handleChange(event)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="port" className="control-label">Port</label>
-                <input type="text" id="port" value={this.state.port} className="form-control" onChange={(event) => this.handleChange(event)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="database" className="control-label">Database</label>
-                <input type="text" id="database" value={this.state.database} className="form-control" onChange={(event) => this.handleChange(event)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="user" className="control-label">User</label>
-                <input type="text" id="user" value={this.state.user} className="form-control" onChange={(event) => this.handleChange(event)} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password" className="control-label">Password</label>
-                <input type="text" id="password" value={this.state.password} className="form-control" onChange={(event) => this.handleChange(event)} />
-              </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-default" onClick={() => this.cancel()}>Cancel</button>
-              <button type="button" className="btn btn-primary" onClick={() => this.save()}>Save</button>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    );
-  }
-
-  handleChange(event) {
-
-    const newState = {};
-
+  const handleChange = event => {
     const key = event.target.id;
     const value = event.target.value;
+    setState(prevState => Object.assign({}, prevState, { [key]: value }));
+  };
 
-    newState[key] = value;
+  const cancel = () => {
+    $(connectionModal.current).modal('hide');
+  };
 
-    this.setState(newState);
-  }
-
-  show() {
-    let newState;
-    try {
-      newState = DatabaseFactory.loadConfig();
-    } catch (error) {
-      newState = EMPTY_STATE;
-    }
-    this.setState(newState);
-    $(this.refs.connectionModal).modal('show');
-  }
-
-  cancel() {
-    $(this.refs.connectionModal).modal('hide');
-  }
-
-  save() {
+  const save = () => {
     const data = {
-      type : this.state.type,
-      host : this.state.host,
-      port : this.state.port,
-      database : this.state.database,
-      user : this.state.user,
-      password : this.state.password
+      type : state.type,
+      host : state.host,
+      port : state.port,
+      database : state.database,
+      user : state.user,
+      password : state.password
     }
-    this.props.onSave(data);
-    $(this.refs.connectionModal).modal('hide');
+    props.onSave(data);
+    $(connectionModal.current).modal('hide');
   }
 
-}
+  useImperativeHandle(ref, () => ({
+    show() {
+      let newState;
+      try {
+        newState = DatabaseFactory.loadConfig();
+      } catch (error) {
+        newState = EMPTY_STATE;
+      }
+      setState(newState);
+      $(connectionModal.current).modal('show');
+    }
+  }));
+
+  return (
+    <div className="modal fade" role="dialog" ref={connectionModal}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <button type="button" className="close" onClick={() => cancel()}>&times;</button>
+            <h4 className="modal-title">Connection</h4>
+          </div>
+          <div className="modal-body">
+            <div className="form-group">
+              <label htmlFor="type" className="control-label">Type</label>
+              <select id="type" value={state.type} className="form-control" onChange={(event) => handleChange(event)}>
+                <option value=""></option>
+                <option value="MySQL">MySQL</option>
+                <option value="PostgreSQL">PostgreSQL</option>
+                <option value="SQLServer">SQLServer</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="host" className="control-label">Host</label>
+              <input type="text" id="host" value={state.host} className="form-control" onChange={(event) => handleChange(event)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="port" className="control-label">Port</label>
+              <input type="text" id="port" value={state.port} className="form-control" onChange={(event) => handleChange(event)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="database" className="control-label">Database</label>
+              <input type="text" id="database" value={state.database} className="form-control" onChange={(event) => handleChange(event)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="user" className="control-label">User</label>
+              <input type="text" id="user" value={state.user} className="form-control" onChange={(event) => handleChange(event)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="control-label">Password</label>
+              <input type="text" id="password" value={state.password} className="form-control" onChange={(event) => handleChange(event)} />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-default" onClick={() => cancel()}>Cancel</button>
+            <button type="button" className="btn btn-primary" onClick={() => save()}>Save</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}));
+
+export default ConnectionModal;
